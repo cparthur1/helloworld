0cc68a0503b2a5491dad4892c7c72e2e12c9237d
cparthur1 <arthur.paludo.c@gmail.com>
2022-04-16 13:17:14 -0300

    added script to redirect to (future) mobile version

---
+++ script.js
@@ -0,0 +1,9 @@
+var delayInMilliseconds = 2000;
+if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+    location.href = './idexm.html';
+} else {
+    console.log("Vc esta usando um computador")
+    setTimeout(function() {
+        console.log("E tb eh gayyy ihhhh")
+    }, delayInMilliseconds);
+}
