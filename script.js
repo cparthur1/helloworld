ad22e20aa41f93a0e0c70baf458b38795514ad0d
cparthur1 <arthur.paludo.c@gmail.com>
2022-04-16 15:15:51 -0300

    debug java error

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var delayInMilliseconds = 2000;
 if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-    location.href = './idexm.html';
+    location.href = './m_index.html';
 } else {
     console.log("Vc esta usando um computador")
     setTimeout(function() {
