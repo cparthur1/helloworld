ef629e7ec4f4fc90212b7eeffeaadbd7f5781128
cparthur1 <arthur.paludo.c@gmail.com>
2022-04-16 16:13:27 -0300

    debug viewer mode script not running

--- script.js
+++ script.js
@@ -7,11 +7,3 @@ if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(naviga
         console.log("E tb eh gayyy ihhhh")
     }, delayInMilliseconds);
 }
-
-{async src="https://www.googletagmanager.com/gtag/js?id=G-PTRZ2T0CQC";
-}
-  {window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-PTRZ2T0CQC');}
