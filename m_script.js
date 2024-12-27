59e60fd372808b69c464ee7541ac8438c827534a
arthur <arthur.paludo.c@gmail.com>
2024-12-27 20:34:06 -0300

    Background music and backup

--- m_script.js
+++ m_script.js
@@ -1,1 +1,9 @@
 console.log("Vc esta usando a versão mobile")
+// m_script.js
+
+document.addEventListener('click', function() {
+    var audio = document.querySelector('audio');
+    audio.play().catch(error => {
+      console.error("Error playing audio:", error);
+    }); 
+  }, { once: true });
